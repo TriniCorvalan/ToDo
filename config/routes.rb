d74ba4fcdc946dc3deb24143e6dd96624addaffc
Trini <trini.4595@gmail.com>
2021-12-14 12:06:24 -0300

    fix(routes): set to do lists index as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "to_do_lists#index"
+  resources :to_do_lists
 end
