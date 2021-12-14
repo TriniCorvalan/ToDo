5661460570f32a9d07c9a0726744997ff5b6203d
Trini <trini.4595@gmail.com>
2021-12-14 14:03:52 -0300

    feat(to_do_item): add complete and uncomplete actions, routes and links

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -1,5 +1,6 @@
 class ToDoItemsController < ApplicationController
   before_action :set_to_do_list
+  before_action :set_to_do_item, except: :create
 
   def create
     if @to_do_item = @to_do_list.to_do_items.create(to_do_item_params)
@@ -18,11 +19,25 @@ class ToDoItemsController < ApplicationController
       flash[:alert] = "No se pudo borrar la tarea"
     end
     redirect_to @to_do_list
-   end
+  end
+
+  def complete
+    @to_do_item.update(completed: true)
+    redirect_to @to_do_list, notice: "Tarea marcada como completada"
+  end
+
+  def uncomplete
+    @to_do_item.update(completed: false)
+    redirect_to @to_do_list, notice: "Tarea marcada como  incompleta"
+  end
 
   private
     def set_to_do_list
       @to_do_list = ToDoList.find(params[:to_do_list_id])
+    end
+
+    def set_to_do_item
+      @to_do_item = @to_do_list.to_do_items.find(params[:id])
     end
 
     def to_do_item_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "to_do_lists#index"
   resources :to_do_lists do
-    resources :to_do_items
+    resources :to_do_items do
+      member do
+        patch :complete
+        patch :uncomplete
+      end
+    end
   end
 end
