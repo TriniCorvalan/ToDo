09422999f54a8c03932f9dfdb6ff1b620f63975a
Trini <trini.4595@gmail.com>
2021-12-14 12:27:41 -0300

    feat(to_do_item): add to do items controller with create action generate to do items controller with create action and set to do list for references update routes nesting to do items resources in to do list routes

---
+++ app/controllers/to_do_items_controller.rb
@@ -0,0 +1,17 @@
+class ToDoItemsController < ApplicationController
+  before_action :set_to_do_list
+
+  def create
+    @to_do_item = @to_do_list.to_do_items.create(to_do_item_params)
+    redirect_to @to_do_list
+  end
+
+  private
+    def set_to_do_list
+      @to_do_list = ToDoList.find(params[:to_do_list_id])
+    end
+
+    def to_do_item_params
+      params[:to_do_item].permit(:name, :completed, :to_do_list_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "to_do_lists#index"
-  resources :to_do_lists
+  resources :to_do_lists do
+    resources :to_do_items
+  end
 end
